1894df0b8ca65694c724b67dc3aa4dc5d42962a3
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-12-05 22:45:24 +0000

    Day 03 (#2)
    * Scaffold
    
    * Basic first tests
    
    * Failing 3 digit test
    
    * Fix edge case where highest number is last
    
    * Fix edge case where highest number is last
    
    * Code to get total joltage (finished pt 1)
    
    * Part 1 writeup

---
+++ src/main/kotlin/day03/Main.kt
@@ -0,0 +1,34 @@
+package day03
+
+import java.io.File
+
+fun main() {
+    val result = File("src/main/kotlin/day03/input.txt").useLines { getTotalJoltage(it.toList()) }
+    println("Total Joltage: $result")
+}
+
+fun getTotalJoltage(banks: List<String>): Int {
+    return banks.sumOf { getMaximumJoltage(it) }
+}
+
+fun getMaximumJoltage(bank: String): Int {
+    var firstJoltage = 0
+    var firstIndex = 0
+    bank.substring(0, bank.length - 1).forEachIndexed { index, joltage ->
+        val joltageValue = joltage.toString().toInt()
+        if (joltageValue > firstJoltage) {
+            firstJoltage = joltageValue
+            firstIndex = index
+        }
+    }
+
+    var secondJoltage = 0
+    bank.substring(firstIndex + 1).forEachIndexed { index, joltage ->
+        val joltageValue = joltage.toString().toInt()
+        if (joltageValue > secondJoltage) {
+            secondJoltage = joltageValue
+        }
+    }
+
+    return "$firstJoltage$secondJoltage".toInt()
+}
---
+++ src/test/kotlin/day03/MainTest.kt
@@ -0,0 +1,49 @@
+package day03
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Nested
+    inner class GetTotalJoltage {
+        @Test
+        fun `acceptance test total joltage from known input and output`() {
+            val banks = listOf(
+                "987654321111111",
+                "811111111111119",
+                "234234234234278",
+                "818181911112111"
+            )
+            val totalJoltage = getTotalJoltage(banks)
+            totalJoltage shouldBe 357
+        }
+    }
+
+    @Nested
+    inner class GetMaximumJoltageTest {
+        @Test
+        fun `a bank with all values the same works`() {
+            val maxJoltage = getMaximumJoltage("111111111111111")
+            maxJoltage shouldBe 11
+        }
+
+        @Test
+        fun `a bank with 2 distinct values works`() {
+            val maxJoltage = getMaximumJoltage("811111111111111")
+            maxJoltage shouldBe 81
+        }
+
+        @Test
+        fun `a bank with 3 distinct values works`() {
+            val maxJoltage = getMaximumJoltage("891111111111111")
+            maxJoltage shouldBe 91
+        }
+
+        @Test
+        fun `a bank where the highest number is the last one works`() {
+            val maxJoltage = getMaximumJoltage("811111111111119")
+            maxJoltage shouldBe 89
+        }
+    }
+}

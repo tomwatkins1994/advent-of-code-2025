49b0172f5ccae1ddb0dee5ab77b3d6060c52e130
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-05 21:47:21 +0000

    Basic first tests

--- src/main/kotlin/day03/Main.kt
+++ src/main/kotlin/day03/Main.kt
@@ -1,5 +1,24 @@
 package day03
 
 fun main() {
-    
+
+}
+
+fun getTotalJoltage(banks: List<String>): Int {
+    return banks.sumOf { getMaximumJoltage(it) }
+}
+
+fun getMaximumJoltage(bank: String): Int {
+    var firstJoltage = 0
+    var firstIndex = 0
+    bank.forEachIndexed { index, joltage ->
+        val joltageValue = joltage.toString().toInt()
+        if (joltageValue > firstJoltage) {
+            firstJoltage = joltageValue
+        }
+    }
+
+    var secondJoltage = 1
+
+    return "$firstJoltage$secondJoltage".toInt()
 }
--- src/test/kotlin/day03/MainTest.kt
+++ src/test/kotlin/day03/MainTest.kt
@@ -1,4 +1,37 @@
 package day03
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class MainTest {
+    @Nested
+    inner class GetTotalJoltage {
+        @Test
+        fun `acceptance test total joltage from known input and output`() {
+            val banks = listOf(
+                "987654321111111",
+                "811111111111119",
+                "234234234234278",
+                "818181911112111"
+            )
+            val totalJoltage = getTotalJoltage(banks)
+            totalJoltage shouldBe 357
+        }
+    }
+
+    @Nested
+    inner class GetMaximumJoltageTest {
+        @Test
+        fun `a bank with all values the same works`() {
+            val maxJoltage = getMaximumJoltage("111111111111111")
+            maxJoltage shouldBe 11
+        }
+
+        @Test
+        fun `a bank with 2 distinct values works`() {
+            val maxJoltage = getMaximumJoltage("811111111111111")
+            maxJoltage shouldBe 81
+        }
+    }
 }

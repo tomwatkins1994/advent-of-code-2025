c20c1ffd9dcb09e2ca5e200c65eb52023905fa28
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-06 17:15:53 +0000

    Convert to long

--- src/main/kotlin/day05/Main.kt
+++ src/main/kotlin/day05/Main.kt
@@ -1,22 +1,25 @@
 package day05
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val result = getFreshAndAvailableIngredients(File("src/main/kotlin/day05/input.txt").readText())
+    println("Fresh and available ingredients: $result")
 }
 
 data class Ingredients(
-    val fresh: Set<Int>,
-    val available: Set<Int>
+    val fresh: Set<Long>,
+    val available: Set<Long>
 )
 
-fun getFreshAndAvailableIngredients(input: String): Set<Int> {
+fun getFreshAndAvailableIngredients(input: String): Set<Long> {
     val ingredients = parseInput(input)
     return ingredients.available.filter { ingredients.fresh.contains(it) }.toSet()
 }
 
 fun parseInput(input: String): Ingredients {
-    val fresh = mutableSetOf<Int>()
-    val available = mutableSetOf<Int>()
+    val fresh = mutableSetOf<Long>()
+    val available = mutableSetOf<Long>()
 
     var inRanges = true
     for (line in input.lines()) {
@@ -26,11 +29,11 @@ fun parseInput(input: String): Ingredients {
         }
         if (inRanges) {
             val range = line.split("-")
-            for (ingredient in range.first().toInt()..range.last().toInt()) {
+            for (ingredient in range.first().toLong()..range.last().toLong()) {
                 fresh.add(ingredient)
             }
         } else {
-            available.add(line.toInt())
+            available.add(line.toLong())
         }
     }
 

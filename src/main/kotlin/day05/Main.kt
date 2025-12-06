aee86b8e9e2cad7973f7ae40cb57f6f8d677ed6a
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-06 17:10:19 +0000

    Handle parsing

--- src/main/kotlin/day05/Main.kt
+++ src/main/kotlin/day05/Main.kt
@@ -4,6 +4,34 @@ fun main() {
 
 }
 
+data class Ingredients(
+    val fresh: Set<Int>,
+    val available: Set<Int>
+)
+
 fun getFreshIngredients(input: String): List<Int> {
     return listOf()
+}
+
+fun parseInput(input: String): Ingredients {
+    val fresh = mutableSetOf<Int>()
+    val available = mutableSetOf<Int>()
+
+    var inRanges = true
+    for (line in input.lines()) {
+        if (line == "") {
+            inRanges = false
+            continue
+        }
+        if (inRanges) {
+            val range = line.split("-")
+            for (ingredient in range.first().toInt()..range.last().toInt()) {
+                fresh.add(ingredient)
+            }
+        } else {
+            available.add(line.toInt())
+        }
+    }
+
+    return Ingredients(fresh = fresh, available = available)
 }
--- src/test/kotlin/day05/MainTest.kt
+++ src/test/kotlin/day05/MainTest.kt
@@ -26,4 +26,43 @@ class MainTest {
             freshIngredients.size shouldBe 3
         }
     }
+
+    @Nested
+    inner class ParseInputTest {
+        @Test
+        fun `gets 1 range and 1 ingredient`() {
+            val input = """
+                3-5
+                
+                1
+            """.trimIndent()
+            val parsedInput = parseInput(input)
+            parsedInput shouldBe Ingredients(fresh = setOf(3, 4, 5), available = setOf(1))
+        }
+
+        @Test
+        fun `handle overlap`() {
+            val input = """
+                3-5
+                4-6
+                
+                1
+            """.trimIndent()
+            val parsedInput = parseInput(input)
+            parsedInput shouldBe Ingredients(fresh = setOf(3, 4, 5, 6), available = setOf(1))
+        }
+
+        fun `gets multiple ranges and ingredients`() {
+            val input = """
+                3-5
+                7-8
+                
+                1
+                2
+                4
+            """.trimIndent()
+            val parsedInput = parseInput(input)
+            parsedInput shouldBe Ingredients(fresh = setOf(3, 4, 5, 7, 8), available = setOf(1, 2, 4))
+        }
+    }
 }

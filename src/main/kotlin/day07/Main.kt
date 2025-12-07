55dbb900870eaec86760bbf1b1d7203a7625cd95
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-07 21:03:08 +0000

    Get starting position

--- src/main/kotlin/day07/Main.kt
+++ src/main/kotlin/day07/Main.kt
@@ -1,5 +1,13 @@
 package day07
 
 fun main() {
-    
+
+}
+
+fun getNumberOfSplits(input: String): Int {
+    return 0
+}
+
+fun getStartingPosition(input: String): Int {
+    return input.lines().first().indexOfFirst { it == 'S' }
 }
